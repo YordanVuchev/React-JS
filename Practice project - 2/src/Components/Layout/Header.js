d6969d1d4ac3f8fe1f246e3d33dd3b7228deb498
Yordan Vuchev <danivu4e@abv.bg>
2022-08-24 21:29:17 +0300

    Started building practice project 2

---
+++ Practice project - 2/src/Components/Layout/Header.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from "react";
+
+import HeaderCartButton from "./HeaderCartButton";
+
+import mealsImg from "../../Assets/meals.jpg";
+import styles from "./Header.module.css";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={styles.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={styles["main-image"]}>
+        <img src={mealsImg} alt="A table full of food" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
