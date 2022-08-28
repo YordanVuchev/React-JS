592a6d9881611a93f060f2e1f5c3a4c1c16304ae
Yordan Vuchev <danivu4e@abv.bg>
2022-08-28 11:07:04 +0300

    Fixed the Backdrop component bug

--- Practice project - 2/src/Components/UI/Modal.js
+++ Practice project - 2/src/Components/UI/Modal.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import styles from "./Modal.module.css";
 
 const Backdrop = (props) => {
-  <div className={styles.backdrop} onClick={props.onClose} />;
+  return <div className={styles.backdrop} onClick={props.onClose} />;
 };
 
 const ModalOverlay = (props) => {
@@ -20,7 +20,7 @@ const Modal = (props) => {
   return (
     <Fragment>
       {ReactDOM.createPortal(
-        <Backdrop onClose={props.onClose} />,
+        <Backdrop onClose={props.onClose}></Backdrop>,
         portalElement
       )}
       {ReactDOM.createPortal(
